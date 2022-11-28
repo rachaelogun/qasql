ff3b88376eea276ffd08b7ea6fbf7ac5b51088ff
Rachael Og <48454465+rachaelogun@users.noreply.github.com>
2022-11-28 16:26:05 +0000

    Add files via upload

--- ImprovedOrderAnalysis.sql
+++ ImprovedOrderAnalysis.sql
@@ -15,3 +15,15 @@ GROUP BY
 	c.CompanyName
 ORDER BY
 	NumOrders
+
+SELECT c.CompanyName, COUNT(o.OrderID) AS NumOrders,
+ISNULL(
+CONVERT(
+VARCHAR(20),
+MIN (o.OrderDate), 106),'None placed') AS MinDate 
+FROM dbo.Customers AS c
+LEFT JOIN dbo.Orders AS o
+On o.CustomerID = c.CustomerID
+WHERE YEAR(o.OrderDate)=1996
+GROUP BY c.CompanyName
+ORDER BY NumOrders
