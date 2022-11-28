a3bf40067f49228fc78c2973d17edbbfc0b76cc2
Rachael Og <48454465+rachaelogun@users.noreply.github.com>
2022-11-28 15:57:32 +0000

    Add files via upload

--- ImprovedDirectory.sql
+++ ImprovedDirectory.sql
@@ -6,3 +6,19 @@ Phone
 
 FROM
 dbo.Customers
+
+
+SELECT CompanyName, Left(ContactName,CHARINDEX('',ContactName)) AS FirstName, SUBSTRING(ContactName, CHARINDEX('',ContactName) +1,50)
+AS LastName, Phone 
+FROM dbo.Customers
+UNION ALL
+
+SELECT CompanyName,
+LEFT(ContactName, CHARINDEX('',ContactName))
+AS FirstName, SUBSTRING (ContactName, CHARINDEX('',ContactName)+1,50)
+AS LastName,
+Phone
+FROM dbo.Suppliers
+UNION ALL
+SELECT 'Northwind Trders', FirstName, LastName, Extension
+FROM dbo.Employees
