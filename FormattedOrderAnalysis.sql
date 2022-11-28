bdded3b5f5be9036d6e2f364bbe41c717d0035da
Rachael Og <48454465+rachaelogun@users.noreply.github.com>
2022-11-28 16:31:15 +0000

    Add files via upload

---
+++ FormattedOrderAnalysis.sql
@@ -0,0 +1,13 @@
+USE Northwind;
+	
+SELECT c.CompanyName, COUNT(o.OrderID) AS NumOrders,
+ISNULL(
+CONVERT(
+VARCHAR(20),
+MIN (o.OrderDate), 106),'None placed') AS MinDate 
+FROM dbo.Customers AS c
+LEFT JOIN dbo.Orders AS o
+On o.CustomerID = c.CustomerID
+WHERE YEAR(o.OrderDate)=1996
+GROUP BY c.CompanyName
+ORDER BY NumOrders
